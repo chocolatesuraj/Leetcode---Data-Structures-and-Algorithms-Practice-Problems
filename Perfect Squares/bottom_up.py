9fb8b51f9a85a08c578e04178642969da117fc92
Suraj Rao <138556273+chocolatesuraj@users.noreply.github.com>
2026-08-21 21:33:17 -0700

    Create bottom_up.py

---
+++ Perfect Squares/bottom_up.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        memo=[float("inf")]*(n+1)
+        memo[0]=0
+        memo[1]=1
+        i=1
+        
+        for i in range (0,n+1):
+            if memo[i]!=1:
+                j=1
+                while(j*j<=i):
+                    memo[i]=min(memo[i],1+memo[i-(j*j)])
+                    j+=1
+        return memo[-1]
